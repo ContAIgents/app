2c83a9302c00a69f556bcd9c1455e7aedd5a1d01
Ajai Chemmanam <ajaichemmanam@gmail.com>
2025-02-11 15:35:05 +0530

    Add export card

--- frontend/src/pages/Export.page.tsx
+++ frontend/src/pages/Export.page.tsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from 'react';
 import { IconLock } from '@tabler/icons-react';
 import { Button, Container, Group, Stack, Text, Textarea, Title, Tooltip } from '@mantine/core';
+import ExportCard from '@/components/Export/ExportCard';
 import { ConfigManager } from '@/services/config/ConfigManager';
-import { Comment, ContentBlock } from '@/types/content';
+import { ContentBlock } from '@/types/content';
 
 export function ExportPage() {
   const [finalContent, setFinalContent] = useState('');
@@ -39,25 +40,67 @@ export function ExportPage() {
           />
 
           <Stack gap="md" align="stretch">
-            <Button onClick={() => handleExport('pdf')}>Export as PDF</Button>
-            <Button onClick={() => handleExport('text')}>Export as Text</Button>
-            <Button onClick={() => handleExport('blog')}>Export as Blog</Button>
-
-            <Tooltip label="Coming Soon!">
-              <Button variant="light" disabled onClick={() => handleExport('podcast')}>
-                Export as Podcast <IconLock size="1rem" />
-              </Button>
-            </Tooltip>
+            <Button onClick={() => handleExport('pdf')}>Export as PDF </Button>
             <Tooltip label="Coming Soon!">
               <Button variant="light" disabled onClick={() => handleExport('image')}>
-                Export as Image <IconLock size="1rem" />
+                Send Newsletter <IconLock size="1rem" />
+              </Button>
+            </Tooltip>
+
+            <ExportCard
+              title="No Limits to Creativity"
+              content="Use generative AI to create impressive videos based on your content"
+              imageSrc="https://siteefy.com/wp-content/uploads/2023/09/Synthesia.png"
+              onBtnClick={() => {}}
+              buttonText="Generate Video"
+              isLocked={true}
+              isPremium={true}
+            />
+            <ExportCard
+              title="Social Together"
+              content="Share your thoughts with your friends and followers"
+              onBtnClick={() => {}}
+              imageSrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPdb_m0eYeQQNGk796cpoGFfcHbCiVm4vRUg&s"
+              buttonText="Share on Social Media"
+              isLocked={true}
+              isPremium={false}
+            />
+            <Tooltip label="Coming Soon!">
+              <Button variant="light" disabled onClick={() => handleExport('image')}>
+                Create Shorts <IconLock size="1rem" />
               </Button>
             </Tooltip>
             <Tooltip label="Coming Soon!">
               <Button variant="light" disabled onClick={() => handleExport('video')}>
-                Export as Video <IconLock size="1rem" />
+                Create Reels <IconLock size="1rem" />
               </Button>
             </Tooltip>
+            <ExportCard
+              title="Create Blog"
+              content="Directly post to Medium "
+              onBtnClick={() => {}}
+              imageSrc="https://miro.medium.com/v2/resize:fit:1400/0*sG6BT7e579CET2QA"
+              buttonText="Create Blog"
+              isLocked={true}
+              isPremium={false}
+            />
+            <ExportCard
+              title="No Limits to Creativity"
+              content="Use generative AI to create an image from your content"
+              onBtnClick={() => {}}
+              buttonText="Generate Image"
+              isLocked={true}
+              isPremium={true}
+            />
+            <ExportCard
+              title="Create Podcast"
+              content="Use Elevenlabs to create a podcast"
+              onBtnClick={() => {}}
+              imageSrc="https://nomusica.com/wp-content/uploads/2024/11/ElevenLabs.jpg"
+              buttonText="Create Podcast"
+              isLocked={true}
+              isPremium={true}
+            />
           </Stack>
         </Group>
       </Stack>
